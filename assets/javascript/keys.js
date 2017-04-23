15b64bbcc9b0e4a354955105de38cd807878fb38
Katie <katiearina@gmail.com>
2017-04-22 20:37:04 -0400

    Added Keys and file structure. :snail:

---
+++ assets/javascript/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'kxohD8NEAU8OAdENrJBSCvsIF',
+  consumer_secret: 'zG8vy1m3vMtWaSn5Mo8yAxkF3LLc0oO2j1SzziLzoFuw51ZMpv',
+  access_token_key: '855174241326706689-ezGKNQuRTJ2LAauFKk6Go1LfNhTYJxp',
+  access_token_secret: 'NZAwtl0n7oiNmjhRHVBYORqDjFCuDoEsjlcyaZgTkwq2L',
+}
