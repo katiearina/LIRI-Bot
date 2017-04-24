841a88dc4a72fbe252f0976a13aa9302da8daf40
Katie <katiearina@gmail.com>
2017-04-23 21:19:03 -0400

    :zzz: Things are working properly; would like to make code drier though. Still need to add comments. Bedtime for now.

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -8,7 +8,12 @@ var accessTokenKey = twitterKeys.twitterKeys.access_token_key;
 var accessTokenSecret = twitterKeys.twitterKeys.access_token_secret;
 // console.log(twitterKeys);
 
-var userInput = process.argv[2];
+var userRequest = process.argv[2];
+
+var Twitter = require("twitter");
+var spotify = require("spotify");
+var request = require('request');
+var fs = require("fs");
 // var multArtists = [];
 
 //---------------------------------------------------------------------------
@@ -17,7 +22,7 @@ var userInput = process.argv[2];
 function myTweets() {
 	// console.log("Tweet!");
 	// console.log(twitterKeys.twitterKeys.consumer_key);
-	var Twitter = require('twitter');
+	// var Twitter = require('twitter');
  
 	var client = new Twitter({
 		consumer_key: consumerKey,
@@ -41,7 +46,7 @@ function myTweets() {
 
 function spotifyThisSong() {
 	// console.log("Spotify'd!");
-var spotify = require('spotify');
+// var spotify = require('spotify');
 var spotifyInput = process.argv[3];
 
 // If there is no input provided by the user,
@@ -101,9 +106,9 @@ spotify.search({type: "track", query: spotifyInput}, function(err, data) {
 
 function movieThis() {
 	// console.log("Movie!");
-	var request = require('request');
-	var userInput = process.argv[3];
-	var movieInput;
+// var request = require('request');
+var userInput = process.argv[3];
+var movieInput;
 
 if (userInput === undefined) {
 	movieInput = "Mr. Nobody";
@@ -152,25 +157,118 @@ else if (userInput !== undefined) {
 }
 
 function doWhatItSays() {
-	console.log("It says!");
+
+// console.log("It says!");
+// var fs = require("fs");
+
+fs.readFile("../../random.txt", "utf8", function(err, data) {
+
+	var output = data.split(",");
+
+	for (var i = 0; i < output.length; i++) {
+		console.log(output[i]);
+	}
+
+	if (output[0].toLowerCase() === "my-tweets") {
+		myTweets();
+	}
+
+	else if (output[0].toLowerCase() === "spotify-this-song") {
+		var spotifyInput = output[1];
+			spotify.search({type: "track", query: spotifyInput}, function(err, data) {
+			    if (err) {
+			        console.log('Error occurred: ' + err);
+			        return;
+			    }
+			 	else {
+			 		console.log("----- Top 3 Results ---------------------------------------------------------------");
+			 		for (var i = 0; i < 3; i++) {
+					// Artist(s) Name
+					// Finesse this multiple artist display more to make it better looking?
+					// Add condition so that if input return is undefined, it returns an error message? (e.g. "Secondhand News")
+					var multArtists = [];
+			 			for (var j = 0; j < data.tracks.items[j].artists.length; j++) { 
+			 				multArtists.push(data.tracks.items[i].artists[j].name);
+							// console.log("Artist(s) Name: " + data.tracks.items[i].artists[0].name + ", " + data.tracks.items[i].artists[1].name);
+							// console.log("Artist(s) Name: " + data.tracks.items[i].artists[j].name);				
+						}
+					console.log("Artist(s) Name(s): " + multArtists.join(", "));
+					// Song Name
+					console.log("Song Name: " + data.tracks.items[i].name);
+					// Preview Link to the song on Spotify
+					console.log("Preview Link: " + data.tracks.items[i].preview_url);
+					// Album Name
+					console.log("Album Name: " + data.tracks.items[i].album.name);
+					console.log("-----");
+					}
+					console.log("--------------------------------------------------------------------");
+			 	}
+			});
+	}
+
+	else if (output[0].toLowerCase() === "movie-this") {
+		movieInput = output[1];
+			var movieSearchUrl = "http://www.omdbapi.com/?t=" + movieInput + "&y=&plot=short&r=json&tomatoes=true";
+
+	request(movieSearchUrl, function(error, response, body) {
+
+		// If there were no errors and the response code was 200 (i.e. the request was successful)...
+		if (!error && response.statusCode === 200) {
+
+			// console.log(movieSearchUrl);
+			console.log("----- Movie Information ---------------------------------------------------------------");
+
+			// Title of the movie
+			console.log("Movie Title: " + JSON.parse(body).Title);
+
+			// Year the movie came out
+			console.log("Release Year: " + JSON.parse(body).Year);
+
+			// IMDB rating of the movie
+			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+
+			// Country where the movie was produced
+			console.log("Country of Production: " + JSON.parse(body).Country);
+
+			// Language of the movie
+			console.log("Language: " + JSON.parse(body).Language);
+
+			// Plot of the movie
+			console.log("Short Plot: " + JSON.parse(body).Plot);
+
+			// Actors in the movie
+			console.log("Actors: " + JSON.parse(body).Actors);
+
+			// Rotten Tomatoes URL
+			console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+			console.log("--------------------------------------------------------------------");
+		}
+	});
+	}
+
+});
+
 }
 
 //---------------------------------------------------------------------------
 // FUNCTION CALLS!
 
-if (userInput.toLowerCase() === "my-tweets") {
+// console.log(process.argv);
+// console.log(process.argv.join(" "));
+
+if (userRequest.toLowerCase() === "my-tweets") {
 	myTweets();
 }
 
-else if (userInput.toLowerCase() === "spotify-this-song") {
+else if (userRequest.toLowerCase() === "spotify-this-song") {
 	spotifyThisSong();
 }
 
-else if (userInput.toLowerCase() === "movie-this") {
+else if (userRequest.toLowerCase() === "movie-this") {
 	movieThis();
 }
 
-else if (userInput.toLowerCase() === "do-what-it-says") {
+else if (userRequest.toLowerCase() === "do-what-it-says") {
 	doWhatItSays();
 }
 
